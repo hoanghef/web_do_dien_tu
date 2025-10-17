bb71e5032c8aef170c9f004062d8c63e51bb764e
thang-cma <23010478@st.phenikaa-uni.edu.vn>
2025-10-17 09:31:10 +0700

    Update 2025_05_21_091313_products.php

--- database/migrations/2025_05_21_091313_products.php
+++ database/migrations/2025_05_21_091313_products.php
@@ -16,6 +16,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
             $table->string('product_name', 255);
             $table->text('orientation');
             $table->text('description');
