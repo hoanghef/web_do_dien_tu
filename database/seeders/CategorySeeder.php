f3724823cf10ad6f56c022de3e5f5ddd49ebd958
thang-cma <23010478@st.phenikaa-uni.edu.vn>
2025-10-17 09:32:48 +0700

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,20 +4,33 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Carbon;
 
 class CategorySeeder extends Seeder
 {
     public function run()
     {
-        Category::create(['category_name' => 'CPU']);
-        Category::create(['category_name' => 'RAM']);
-        Category::create(['category_name' => 'Mainboard']);
-        Category::create(['category_name' => 'VGA']);
-        Category::create(['category_name' => 'Ổ cứng SSD']);
-        Category::create(['category_name' => 'Nguồn']);
-        Category::create(['category_name' => 'Case']);
-        Category::create(['category_name' => 'Tản nhiệt']);
-        Category::create(['category_name' => 'Phụ kiện']);
+        $now = Carbon::create(2025, 10, 8, 16, 43, 30);
+
+        $categories = [
+            ['category_name' => 'CPU', 'slug' => 'cpu'],
+            ['category_name' => 'RAM', 'slug' => 'ram'],
+            ['category_name' => 'Mainboard', 'slug' => 'motherboard'],
+            ['category_name' => 'VGA', 'slug' => 'gpu'],
+            ['category_name' => 'Ổ cứng SSD', 'slug' => 'ssd'],
+            ['category_name' => 'Nguồn', 'slug' => 'PSU'],
+            ['category_name' => 'Case', 'slug' => 'case'],
+            ['category_name' => 'Tản nhiệt', 'slug' => 'cooler'],
+            ['category_name' => 'Phụ kiện', 'slug' => 'peripherals'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'category_name' => $category['category_name'],
+                'slug' => $category['slug'],
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+        }
     }
 }
